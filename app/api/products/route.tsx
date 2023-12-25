66eb29de993bc9251be04fb082200c0492c13744
Karthic Raja <karthicraja@neulancers.com>
2023-12-25 19:08:57 +0530

    Created basic prisma Model

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+import { schema } from "./schema";
+import { error } from "console";
 
 export function GET(request: NextRequest) {
 	return NextResponse.json([
@@ -15,7 +17,13 @@ export function GET(request: NextRequest) {
 	]);
 }
 
-export async function POST(request: NextRequest){
-    const body = await request.json()
-    return NextResponse.json(body)
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+	const validation = schema.safeParse(body);
+	if (!validation.success)
+		return NextResponse.json(
+			{ error: validation.error.errors },
+			{ status: 400 }
+		);
+	return NextResponse.json({id:2, ...body}, {status:201});
 }
