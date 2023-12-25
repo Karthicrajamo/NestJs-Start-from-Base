f8254f17da588a3a759976dba8efcd2d0270b44e
Karthic Raja <karthicraja@neulancers.com>
2023-12-25 23:24:08 +0530

    POST request

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -5,15 +5,38 @@ import { prisma } from "@/prisma/client";
 
 // (request: NextRequest) is used to Not Cache data
 export async function GET(request: NextRequest) {
-	const user = await prisma.user.findMany()
+	const user = await prisma.user.findMany();
 	return NextResponse.json(user);
 }
 
 export async function POST(request: NextRequest) {
 	const body = await request.json();
-	const validation = schema.safeParse(body)
-	// if (!body.name)
+	const validation = schema.safeParse(body);
 	if (!validation.success)
-		return NextResponse.json({ error: validation.error.errors }, { status: 404 });
-	return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+		return NextResponse.json(
+			{ error: validation.error.errors },
+			{ status: 404 }
+		);
+
+	const user = await prisma.user.findUnique({
+		where: { email: body.email },
+	});
+
+	if (user) {
+		return NextResponse.json({ error: "User already exists" }, { status: 400 });
+	}
+
+	const NewUser = await prisma.user.create({
+		data: { name: body.name, email: body.email },
+	});
+	return NextResponse.json(NewUser, { status: 201 });
 }
+
+// export async function POST(request: NextRequest) {
+// 	const body = await request.json();
+// 	const validation = schema.safeParse(body)
+// 	// if (!body.name)
+// 	if (!validation.success)
+// 		return NextResponse.json({ error: validation.error.errors }, { status: 404 });
+// 	return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+// }
