5a847b35dfe2f40259673787c25e7025b37ec286
Karthic Raja <karthicraja@neulancers.com>
2023-12-26 14:42:50 +0530

    Adding options

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -2,34 +2,37 @@
 import React, { useState } from "react";
 import { CldImage, CldUploadWidget } from "next-cloudinary";
 
-interface CloudinaryResult{
+interface CloudinaryResult {
 	public_id: string;
 }
 
 const UploadPage = () => {
 	const [publicId, setPublicId] = useState("");
 
-	return (<>
-		{publicId && <CldImage src={publicId} alt="cloud image" width={200} height={200}/>}
-		<CldUploadWidget
-			uploadPreset="n96e6o89"
-			onUpload={(result) => {
-				console.log(result);
-				if (result.event !== 'success') return;
-				const info = result.info as CloudinaryResult;
-				setPublicId(info.public_id)
-				
-			}}
+	return (
+		<>
+			{publicId && (
+				<CldImage src={publicId} alt="cloud image" width={200} height={200} />
+			)}
+			<CldUploadWidget
+				options={{ sources: ["local"], multiple: false, maxFiles: 5 }}
+				uploadPreset="n96e6o89"
+				onUpload={(result) => {
+					console.log(result);
+					if (result.event !== "success") return;
+					const info = result.info as CloudinaryResult;
+					setPublicId(info.public_id);
+				}}
 			>
-			{({ open }) => {
-				return (
-					<button className="btn btn-primary" onClick={() => open()}>
-						Upload an Image
-					</button>
-				);
-			}}
-		</CldUploadWidget>
-			</>
+				{({ open }) => {
+					return (
+						<button className="btn btn-primary" onClick={() => open()}>
+							Upload an Image
+						</button>
+					);
+				}}
+			</CldUploadWidget>
+		</>
 	);
 };
 
