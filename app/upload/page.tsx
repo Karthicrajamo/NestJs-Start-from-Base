22a96f64ecf97a5b529c1e475e022a6c730f7706
Karthic Raja <karthicraja@neulancers.com>
2023-12-26 14:32:28 +0530

    cloudinary image upload button

---
+++ app/upload/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState } from "react";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+
+interface CloudinaryResult{
+	public_id: string;
+}
+
+const UploadPage = () => {
+	const [publicId, setPublicId] = useState("");
+
+	return (<>
+		{publicId && <CldImage src={publicId} alt="cloud image" width={200} height={200}/>}
+		<CldUploadWidget
+			uploadPreset="n96e6o89"
+			onUpload={(result) => {
+				console.log(result);
+				if (result.event !== 'success') return;
+				const info = result.info as CloudinaryResult;
+				setPublicId(info.public_id)
+				
+			}}
+			>
+			{({ open }) => {
+				return (
+					<button className="btn btn-primary" onClick={() => open()}>
+						Upload an Image
+					</button>
+				);
+			}}
+		</CldUploadWidget>
+			</>
+	);
+};
+
+export default UploadPage;
