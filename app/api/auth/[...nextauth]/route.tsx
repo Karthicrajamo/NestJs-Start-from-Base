5d600b3c9f66c41165b2dc2feca7c39ca93edcdd
Karthic Raja <karthicraja@neulancers.com>
2023-12-27 15:15:04 +0530

    Setup prisma adopter from provider and create database models to view user dates

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,12 +1,20 @@
-import nextAuth from "next-auth";
+import { prisma } from "@/prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import nextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {providers: [
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
   GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!
   })
-]}
+],
+  session:{
+    strategy: 'jwt'
+  }
+}
 
 const handler = nextAuth(authOptions)
 
