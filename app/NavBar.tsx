b7376c131af3d90c00eef832912a8ce6c845096b
Karthic Raja <karthicraja@neulancers.com>
2023-12-27 13:37:52 +0530

    Add session login to view pages

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,39 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useSession } from "next-auth/react";
 
 const NavBar = () => {
-  return (
-    <div className='flex bg-slate-200'>
-        <Link href={'/'} className='mr-5'>NextJs</Link>
-        <Link href={'/users'}>User</Link>
-    </div>
-  )
-}
+	const { status, data: session } = useSession();
 
-export default NavBar
+	// if (status === 'loading') return;
+
+	return (
+		<div className="flex bg-slate-200 space-x-3 justify-between items-center p-4">
+			<div>
+				<Link href={"/"} className="mr-5">
+					NextJs
+				</Link>
+				<Link href={"/users"}>User</Link>
+			</div>
+			<div >
+				{status === "loading" && (
+					<button className="btn">
+						<span className="loading loading-spinner"></span>
+						loading
+					</button>
+				)}
+				{status === "authenticated" && (
+					<div className="btn btn-info">{session.user!.name}</div>
+				)}
+				{status === "unauthenticated" && (
+					<Link className="btn btn-primary " href={"/api/auth/signin"}>
+						LOGIN
+					</Link>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default NavBar;
