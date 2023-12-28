2f55a7fe5ae1ceb74cf849a5d6726c55ca7ffe3d
Karthic Raja <karthicraja@neulancers.com>
2023-12-28 12:54:08 +0530

    Authorize user input credentials using next-auth credentials and registering users and validate user credentials using next-auth credentials

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -2,20 +2,49 @@ import { prisma } from "@/prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import nextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-  GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID!,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-  })
-],
-  session:{
-    strategy: 'jwt'
-  }
-}
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				email: { lable: "Email", type: "email", placeholder: "Email" },
+				password: {
+					label: "Password",
+					type: "password",
+					placeholder: "Password",
+				},
+			},
+			async authorize(credentials, req) {
+				if (!credentials?.email || !credentials.password) return null;
 
-const handler = nextAuth(authOptions)
+				const user = await prisma.user.findUnique({
+					where: { email: credentials.email },
+				});
 
-export {handler as GET, handler as POST}
+				if (!user) return null;
+
+				const passwordMatches = await bcrypt.compare(
+					credentials.password,
+					user.hashedPassword!
+				);
+
+				return passwordMatches ? user : null;
+			},
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		}),
+	],
+	session: {
+		strategy: "jwt",
+	},
+};
+
+const handler = nextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { getServerSession } from 'next-auth'
-import Image from 'next/image'
-import { authOptions } from './api/auth/[...nextauth]/route'
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
-  return (
-    <h1 className='btn btn-primary'>hai {session && session.user!.name}</h1>
-  )
+	const session = await getServerSession(authOptions);
+	return (
+		<>
+			<h1 className="btn btn-primary">hai {session && session.user!.name}</h1>
+
+		</>
+	);
 }
